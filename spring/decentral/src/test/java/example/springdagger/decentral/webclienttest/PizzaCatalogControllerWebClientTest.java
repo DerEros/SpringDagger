2783dcd548da450f8f18c42633bc6b3ec2bf3e89
DerEros <DerEros@users.noreply.github.com>
2018-05-19 22:03:32 +0200

    Add test that uses full REST and JSON serialization stack

---
+++ spring/decentral/src/test/java/example/springdagger/decentral/webclienttest/PizzaCatalogControllerWebClientTest.java
@@ -0,0 +1,104 @@
+package example.springdagger.decentral.webclienttest;
+
+import example.springdagger.decentral.data.IngredientsDAO;
+import example.springdagger.decentral.util.FakeIngredients;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.inject.Inject;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest
+@AutoConfigureWebTestClient
+@Tag("WebClient")
+class PizzaCatalogControllerWebClientTest {
+
+    @Inject private WebTestClient client;
+
+    @MockBean private IngredientsDAO mockIngredientsDAO;
+
+    private FakeIngredients fakeIngredients = new FakeIngredients();
+
+    @Test
+    void testReadAllIngredients() {
+        String expectedResult = "[\n" +
+                "  {\n" +
+                "    \"id\": 1,\n" +
+                "    \"ingredientType\": \"DOUGH\",\n" +
+                "    \"name\": \"Thin Dough\",\n" +
+                "    \"price\": 1.00\n" +
+                "  }, {\n" +
+                "    \"id\": 2,\n" +
+                "    \"ingredientType\": \"DOUGH\",\n" +
+                "    \"name\": \"Thick Dough\",\n" +
+                "    \"price\": 1.50\n" +
+                "  }, {\n" +
+                "    \"id\": 3,\n" +
+                "    \"ingredientType\": \"SAUCE\",\n" +
+                "    \"name\": \"Tomato Sauce\",\n" +
+                "    \"price\": 0.50\n" +
+                "  }, {\n" +
+                "    \"id\": 4,\n" +
+                "    \"ingredientType\": \"SAUCE\",\n" +
+                "    \"name\": \"BBQ Sauce\",\n" +
+                "    \"price\": 0.70\n" +
+                "  }, {\n" +
+                "    \"id\": 5,\n" +
+                "    \"ingredientType\": \"TOPPING\",\n" +
+                "    \"name\": \"Cheese\",\n" +
+                "    \"price\": 0\n" +
+                "  }, {\n" +
+                "    \"id\": 6,\n" +
+                "    \"ingredientType\": \"TOPPING\",\n" +
+                "    \"name\": \"Salami\",\n" +
+                "    \"price\": 0.50\n" +
+                "  }\n" +
+                "]";
+
+        given(mockIngredientsDAO.getAllIngredients())
+                .willReturn(Flux.fromIterable(fakeIngredients.getAllIngredients()));
+        client.get().uri("/ingredients").exchange().expectStatus().isOk().expectBody().json(expectedResult);
+    }
+
+    @Test
+    void testReadAllIngredientsWhenNoneAvailable() {
+        given(mockIngredientsDAO.getAllIngredients()).willReturn(Flux.empty());
+        client.get().uri("/ingredients").exchange().expectStatus().isOk().expectBody().json("[]");
+    }
+
+    @Test
+    void testRequestIngredientById() {
+        String expectedResult = "  {\n" +
+                "    \"id\": 4,\n" +
+                "    \"ingredientType\": \"SAUCE\",\n" +
+                "    \"name\": \"BBQ Sauce\",\n" +
+                "    \"price\": 0.70\n" +
+                "  }\n";
+
+        given(mockIngredientsDAO.getIngredientById(4L))
+                .willReturn(Mono.just(fakeIngredients.getIngredientById(4L)));
+        client.get().uri("/ingredients/4").exchange().expectStatus().isOk().expectBody().json(expectedResult);
+    }
+
+    @Test
+    @Disabled
+    void testRequestNonExistingIngredientById() {
+        String expectedResult = "{\n" +
+                "  \"error\": \"Ingredient with ID 9 not found\"\n" +
+                "}";
+
+        given(mockIngredientsDAO.getIngredientById(9L)).willReturn(Mono.empty());
+        client.get().uri("/ingredients/9").exchange().expectStatus().isNotFound().expectBody().json(expectedResult);
+    }
+}
---
+++ spring/decentral/src/test/java/example/springdagger/decentral/webclienttest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Tests that use a web client to access the pizza order service; think of them as integration tests.
+ */
+package example.springdagger.decentral.webclienttest;
